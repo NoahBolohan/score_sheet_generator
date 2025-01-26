5a5b6c07a5ece441069c8f91cc11664becc110cc
Noah Bolohan <noah.bolohan@gmail.com>
2025-01-26 11:05:34 -0500

    Some working jinja variables

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# score_sheet_generator
-Generate score sheets for appropriate games.
+# Score Sheet Generator
+
+Generate score sheets for appropriate games using [Jinja2](https://pypi.org/project/Jinja2/).
+
+Features:
+
++ Variable player count
++ Arbitrary row names and counts
++ Post scores to Google Sheet document
---
+++ generate_html.py
@@ -0,0 +1,28 @@
+from jinja2 import Environment, FileSystemLoader
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+score_sheet_template = environment.get_template("score_sheet_template.jinja")
+
+output_fp = "score_sheet.html"
+
+data = {
+    "min_players" : 2,
+    "max_players" : 5,
+    "row_headers" : [
+        "\"row_markers_on_dragon_guild\"",
+        "\"row_printed_on_dragons\"",
+        "\"row_end-game_abilities\"",
+        "\"row_eggs\"",
+        "\"row_cached_resources\"",
+        "\"row_tucked_cards\"",
+        "\"row_public_objectives\"",
+        "\"row_remaining_resources\"",
+        "\"row_total\""
+    ],
+    "homepage_link" : "\"https://noahbolohan.github.io/wyrmspan-utilities/index.html\"",
+    "google_scripts_link" : "\"https://script.google.com/macros/s/AKfycbwHfda9GFnKFvMem6YLQxmeTCjQYSASG1E46ulA6-cmWxoSO137pCEl2m-luSEp9nAmpg/exec\""
+}
+
+with open(output_fp, mode="w", encoding="utf-8") as results:
+    results.write(score_sheet_template.render(data))
+    print(f"... wrote {output_fp}")
